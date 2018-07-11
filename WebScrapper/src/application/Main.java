1e14265705118fdbda0bd7d1285346ec084f5fa2
porphiros <darkrisingforce@gmail.com>
2018-07-11 23:47:49 +0300

    add gradle

--- WebScrapper/src/application/Main.java
+++ WebScrapper/src/application/Main.java
@@ -1,5 +1,5 @@
 package application;
-	
+
 import java.util.List;
 
 import javafx.application.Application;
@@ -18,7 +18,6 @@ import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
 import javafx.stage.Stage;
 
-
 public class Main extends Application {
 	Label lblWebPage;
 	TextField tfWebAddress;
@@ -28,96 +27,80 @@ public class Main extends Application {
 	ScrollPane spTextArea;
 	TextFlow tflowLinks;
 	ScrollPane spTextFlow;
+
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,1280,800);
-			
+			Scene scene = new Scene(root, 1280, 800);
+
 			lblWebPage = new Label("Page Address");
 			tfWebAddress = new TextField("");
 			tfWebAddress.setPrefWidth(1000);
-			
+
 			btnScrape = new Button("Scrape");
 			hbAddress = new HBox(5.0, lblWebPage, tfWebAddress, btnScrape);
 			hbAddress.prefWidthProperty().bind(root.widthProperty());
 			root.setTop(hbAddress);
-			
-			taLinks = new TextArea();			
+
+			taLinks = new TextArea();
 			spTextArea = new ScrollPane(taLinks);
-			/*spTextArea.prefHeightProperty().bind(taLinks.heightProperty());
-			spTextArea.prefWidthProperty().bind(taLinks.widthProperty());*/
+			/*
+			 * spTextArea.prefHeightProperty().bind(taLinks.heightProperty());
+			 * spTextArea.prefWidthProperty().bind(taLinks.widthProperty());
+			 */
 			spTextArea.setFitToWidth(true);
 			spTextArea.setFitToHeight(true);
 			taLinks.prefHeightProperty().bind(root.heightProperty());
 			taLinks.prefWidthProperty().bind(root.widthProperty());
 			taLinks.setFont(Font.font("Arial", 15));
-			
+
 			tflowLinks = new TextFlow();
 			tflowLinks.prefHeightProperty().bind(root.heightProperty());
 			tflowLinks.prefWidthProperty().bind(root.widthProperty());
 			spTextFlow = new ScrollPane(tflowLinks);
 			spTextFlow.setFitToWidth(true);
-			//spTextFlow.setFitToHeight(true);
-			//tflowLinks.setLineSpacing(1.5);		
-			
-			//root.setCenter(spTextArea);
+			// spTextFlow.setFitToHeight(true);
+			// tflowLinks.setLineSpacing(1.5);
+
+			// root.setCenter(spTextArea);
 			root.setCenter(spTextFlow);
-			
+
 			btnScrape.setOnAction(e -> {
-				//getLinksToTextArea();
+				// getLinksToTextArea();
 				getLinksAsHyperLinks();
 			});
-			
+
 			tfWebAddress.setOnKeyPressed(k -> {
-				if(k.getCode() == KeyCode.ENTER) {
+				if (k.getCode() == KeyCode.ENTER) {
 					getLinksAsHyperLinks();
 				}
 			});
-			
-			
-			
-			
+
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.setTitle("Web Scrapper 0.1");
 			primaryStage.show();
-		} catch(Exception e) {
+		} catch (Exception e) {
 			System.err.println(e.getMessage());
 		}
 	}
 
 	private void getLinksAsHyperLinks() {
-		if(tfWebAddress.getText().equals("")) {					
-		}
-		else {		
+		if (tfWebAddress.getText().equals("")) {
+		} else {
 			tflowLinks.getChildren().clear();
-			WebScrapper webScrapper = new WebScrapper(tfWebAddress.getText());	
-			webScrapper.setHostService(getHostServices());					
+			WebScrapper webScrapper = new WebScrapper(tfWebAddress.getText());
+			webScrapper.setHostService(getHostServices());
 			List<Hyperlink> list = webScrapper.collectLinksAsHL();
-			//tflowLinks.getChildren().addAll(list);
-			for(Hyperlink h : list) {
+			// tflowLinks.getChildren().addAll(list);
+			for (Hyperlink h : list) {
 				Text t = new Text("\n");
 				tflowLinks.getChildren().addAll(h, t);
 			}
 		}
-	}
+	}	
 
-	private void getLinksToTextArea() {
-		if(tfWebAddress.getText().equals("")) {
-			taLinks.setText("Please enter a link in the address bar");
-		}
-		else {					
-			taLinks.clear();
-			//System.out.println(tfWebAddress.getText());
-			WebScrapper webScrapper = new WebScrapper(tfWebAddress.getText());
-			//String str = webScrapper.collectLinks();
-			//System.out.println(str);
-			taLinks.setText(webScrapper.collectLinks());
-			
-		}
-	}
-	
 	public static void main(String[] args) {
 		launch(args);
 	}
--- WebScrapper/src/application/WebScrapper.java
+++ WebScrapper/src/application/WebScrapper.java
@@ -11,17 +11,38 @@ import org.jsoup.select.Elements;
 import javafx.application.HostServices;
 import javafx.scene.control.Hyperlink;
 
+/**
+ * {@link WebScrapper} class will make all the required operation to scrape a
+ * web page
+ * 
+ * @author porphiros
+ *
+ */
 public class WebScrapper {
 	private String linkToScrape;
+	// Document element of JSoup to hold the document to scrape
 	private Document document;
+	// default link will point to Google
 	private static final String DEFAULT_LINK = "https://www.google.com/";
-	
+	/**
+	 * {@link HostServices} is used to open links scrapped on the system default
+	 * browser
+	 */
 	private HostServices hostService;
-	
+
+	/**
+	 * 
+	 * @return the host service
+	 */
 	public HostServices getHostService() {
 		return hostService;
 	}
 
+	/**
+	 * Set the host service
+	 * 
+	 * @param hostService
+	 */
 	public void setHostService(HostServices hostService) {
 		this.hostService = hostService;
 	}
@@ -30,93 +51,151 @@ public class WebScrapper {
 		return linkToScrape;
 	}
 
+	/**
+	 * Set the link to scrape
+	 * 
+	 * @param linkToScrape
+	 */
 	public void setLinkToScrape(String linkToScrape) {
-		this.linkToScrape = linkToScrape;		
+		this.linkToScrape = linkToScrape;
 	}
 
+	/**
+	 * Default constructor hooked on the default link
+	 */
 	public WebScrapper() {
 		this(DEFAULT_LINK);
 	}
-	
+
+	/**
+	 * Construct the {@link WebScrapper} to point to a non-default link
+	 * 
+	 * @param linkToScrape
+	 */
 	public WebScrapper(String linkToScrape) {
-		this.linkToScrape = linkToScrape;		
+		this.linkToScrape = linkToScrape;
 		try {
 			this.document = setUpDocument(this.linkToScrape);
 		} catch (IOException e) {
 			System.err.println(e.getMessage());
 		}
 	}
-	
+
+	/**
+	 * Helper method to setup the Jsoup connection using method
+	 * {@link Jsoup.connect()}
+	 * 
+	 * @param link to hook on
+	 * @return a connected document element
+	 * @throws IOException
+	 */
 	private Document setUpDocument(String link) throws IOException {
 		return Jsoup.connect(link).get();
 	}
-	
+
+	/**
+	 * Collect links calls a helper method on the available initialized document
+	 * 
+	 * @return string representation of all links collected
+	 */
 	public String collectLinks() {
 		return collectLinks(this.document);
 	}
-	
+
+	/**
+	 * Helper method to {@link WebScrapper.collectLinks()} will return a string
+	 * representation of links collected
+	 * 
+	 * @param document
+	 * @return
+	 */
 	private String collectLinks(Document document) {
 		StringBuilder sb = new StringBuilder();
 		Document doc = document;
+		/**
+		 * links selector using {@link Jsoup.Document.select()} method the pattern here
+		 * is {code a[href]}
+		 */
 		Elements links = doc.select("a[href]");
+		/**
+		 * for media elements we collect all elements that have a [src] tag
+		 */
 		Elements media = doc.select("[src]");
+		/**
+		 * imports are collected with the pattern link[href]
+		 */
 		Elements imports = doc.select("link[href]");
 
-		for(Element m : media) {
+		// Collect the media elements
+		for (Element m : media) {
 			String str = String.format("%s : %s%n", m.tagName(), m.attr("abs:src"));
 			sb.append(str);
 		}
-		
-		for(Element link : links) {
-			String str = String.format("%s : %s%n",
-					link.tagName(),link.attr("abs:href"));
+
+		for (Element link : links) {
+			String str = String.format("%s : %s%n", link.tagName(), link.attr("abs:href"));
 			sb.append(str);
 		}
-		
-		for(Element link : imports) {
-			String str = String.format("%s : %s (%s)%n",
-					link.tagName(),link.attr("abs:href"), link.attr("rel"));
+
+		for (Element link : imports) {
+			String str = String.format("%s : %s (%s)%n", link.tagName(), link.attr("abs:href"), link.attr("rel"));
 			sb.append(str);
 		}
 
 		return sb.toString();
 	}
-	
-	public List<Hyperlink> collectLinksAsHL(){
+
+	/**
+	 * Collect Links as HyperLinks, calls a helper method
+	 * 
+	 * @return
+	 */
+	public List<Hyperlink> collectLinksAsHL() {
 		return this.collectLinksAsHL(document);
 	}
-	
-	private List<Hyperlink> collectLinksAsHL(Document document){
+
+	private List<Hyperlink> collectLinksAsHL(Document document) {
 		Document doc = document;
 		Elements links = doc.select("a[href]");
 		Elements media = doc.select("[src]");
 		Elements imports = doc.select("link[href]");
 		List<Hyperlink> list = new ArrayList<>();
-		
-		for(Element m : media) {
+
+		for (Element m : media) {
+			/**
+			 * Creates a hyperlink on each media element with attaching the src attribute
+			 */
 			Hyperlink hl = new Hyperlink(m.attr("abs:src"));
+			// Add the hyperlink to the List
 			list.add(hl);
+			/**
+			 * Attach an onclick hyperlink to each element in the list
+			 */
 			hl.setOnAction(e -> {
+				/**
+				 * Call the hostService {@link HostServices.showDocument()} method to open the
+				 * Hyperlink location on click
+				 */
 				hostService.showDocument(hl.getText());
 			});
 		}
-		
-		for(Element link : links) {
+
+		for (Element link : links) {
 			Hyperlink hl = new Hyperlink(link.attr("abs:href"));
 			list.add(hl);
 			hl.setOnAction(e -> {
 				hostService.showDocument(hl.getText());
 			});
 		}
-		
-		for(Element link : imports) {
+
+		for (Element link : imports) {
 			Hyperlink hl = new Hyperlink(link.attr("abs:href"));
 			list.add(hl);
 			hl.setOnAction(e -> {
 				hostService.showDocument(hl.getText());
 			});
 		}
-		
+
 		return list;
 	}
 
